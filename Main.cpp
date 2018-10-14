7ad3467b31fb3b5edb3021021098a8a5c7910fc8
maxeonyx <maxeonyx@gmail.com>
2018-10-14 16:22:35 +1300

    Half of task 1 (load BVH files and play animation)

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,8 @@
 //  COSC422: Advanced Computer Graphics;  University of Canterbury (2018)
 //
 //  FILE NAME: ModelLoader.cpp
-//  Uses the Assimp and DevIL libraries.   
-//  
+//  Uses the Assimp and DevIL libraries.
+//
 //	Most models are created with the z-axis as the primary axis and will
 //  require a 90 degs rotation  about the x-axis for proper display.
 //  Press key '1' to toggle 90 degs model rotation about x-axis on/off.
@@ -23,17 +23,18 @@ using namespace std;
 #include <assimp/postprocess.h>
 #include "assimp_extras.h"
 
-const aiScene* scene = NULL;
+const aiScene *scene = NULL;
 GLuint scene_list = 0;
 float angle = 0;
 aiVector3D scene_min, scene_max;
 bool modelRotn = true;
 std::map<int, int> texIdMap;
 
-bool loadModel(const char* fileName)
+bool loadModel(const char *fileName)
 {
-	scene = aiImportFile(fileName, aiProcessPreset_TargetRealtime_MaxQuality);
-	if(scene == NULL) exit(1);
+	scene = aiImportFile(fileName, aiProcessPreset_TargetRealtime_MaxQuality | aiProcess_Debone);
+	if (scene == NULL)
+		exit(1);
 	//printSceneInfo(scene);
 	//printTreeInfo(scene->mRootNode);
 	//printBoneInfo(scene);
@@ -42,8 +43,7 @@ bool loadModel(const char* fileName)
 	return true;
 }
 
-
-void loadGLTextures(const aiScene* scene)
+void loadGLTextures(const aiScene *scene)
 {
 
 	/* initialization of DevIL */
@@ -58,7 +58,7 @@ void loadGLTextures(const aiScene* scene)
 	/* Simplified version: Retrieves only the first texture with index 0 if present*/
 	for (unsigned int m = 0; m < scene->mNumMaterials; ++m)
 	{
-		aiString path;  // filename
+		aiString path; // filename
 
 		if (scene->mMaterials[m]->GetTexture(aiTextureType_DIFFUSE, 0, &path) == AI_SUCCESS)
 		{
@@ -67,11 +67,11 @@ void loadGLTextures(const aiScene* scene)
 			GLuint texId;
 			ilGenImages(1, &imageId);
 			glGenTextures(1, &texId);
-			texIdMap[m] = texId;   //store tex ID against material id in a hash map
+			texIdMap[m] = texId;  //store tex ID against material id in a hash map
 			ilBindImage(imageId); /* Binding of DevIL image name */
 			ilEnable(IL_ORIGIN_SET);
 			ilOriginFunc(IL_ORIGIN_LOWER_LEFT);
-			if (ilLoadImage((ILstring)path.data))   //if success
+			if (ilLoadImage((ILstring)path.data)) //if success
 			{
 				/* Convert image to RGBA */
 				ilConvertImage(IL_RGBA, IL_UNSIGNED_BYTE);
@@ -81,8 +81,8 @@ void loadGLTextures(const aiScene* scene)
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, ilGetInteger(IL_IMAGE_WIDTH),
-					ilGetInteger(IL_IMAGE_HEIGHT), 0, GL_RGBA, GL_UNSIGNED_BYTE,
-					ilGetData());
+							 ilGetInteger(IL_IMAGE_HEIGHT), 0, GL_RGBA, GL_UNSIGNED_BYTE,
+							 ilGetData());
 				glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
 				cout << "Texture:" << path.data << " successfully loaded." << endl;
 				glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
@@ -92,41 +92,38 @@ void loadGLTextures(const aiScene* scene)
 			}
 			else
 			{
-				cout << "Couldn't load Image: %s\n" << path.data << endl;
+				cout << "Couldn't load Image: %s\n"
+					 << path.data << endl;
 			}
-
 		}
-	}  //loop for material
-
+	} //loop for material
 }
 
-
 // ------A recursive function to traverse scene graph and render each mesh----------
-void render (const aiScene* sc, const aiNode* nd)
+void render(const aiScene *sc, const aiNode *nd)
 {
 	aiMatrix4x4 m = nd->mTransformation;
-	aiMesh* mesh;
-	aiFace* face;
+	aiMesh *mesh;
+	aiFace *face;
 	GLuint texId;
 	int meshIndex, materialIndex;
 
-	aiTransposeMatrix4(&m);   //Convert to column-major order
+	aiTransposeMatrix4(&m); //Convert to column-major order
 	glPushMatrix();
-	glMultMatrixf((float*)&m);   //Multiply by the transformation matrix for this node
+	glMultMatrixf((float *)&m); //Multiply by the transformation matrix for this node
 
 	// Draw all meshes assigned to this node
 	for (int n = 0; n < nd->mNumMeshes; n++)
 	{
-		meshIndex = nd->mMeshes[n];          //Get the mesh indices from the current node
-		mesh = scene->mMeshes[meshIndex];    //Using mesh index, get the mesh object
+		meshIndex = nd->mMeshes[n];		  //Get the mesh indices from the current node
+		mesh = scene->mMeshes[meshIndex]; //Using mesh index, get the mesh object
 
-		apply_material(sc->mMaterials[mesh->mMaterialIndex]);  //Change opengl state to that material's properties 
+		apply_material(sc->mMaterials[mesh->mMaterialIndex]); //Change opengl state to that material's properties
 
-		if(mesh->HasNormals())
+		if (mesh->HasNormals())
 			glEnable(GL_LIGHTING);
 		else
 			glDisable(GL_LIGHTING);
-
 
 		if (mesh->HasVertexColors(0))
 		{
@@ -136,29 +133,36 @@ void render (const aiScene* sc, const aiNode* nd)
 		else
 			glDisable(GL_COLOR_MATERIAL);
 
-
 		//Get the polygons from each mesh and draw them
 		for (int k = 0; k < mesh->mNumFaces; k++)
 		{
 			face = &mesh->mFaces[k];
 			GLenum face_mode;
 
-			switch(face->mNumIndices)
+			switch (face->mNumIndices)
 			{
-				case 1: face_mode = GL_POINTS; break;
-				case 2: face_mode = GL_LINES; break;
-				case 3: face_mode = GL_TRIANGLES; break;
-				default: face_mode = GL_POLYGON; break;
+			case 1:
+				face_mode = GL_POINTS;
+				break;
+			case 2:
+				face_mode = GL_LINES;
+				break;
+			case 3:
+				face_mode = GL_TRIANGLES;
+				break;
+			default:
+				face_mode = GL_POLYGON;
+				break;
 			}
 
 			glBegin(face_mode);
 
-			for(int i = 0; i < face->mNumIndices; i++) {
-				int vertexIndex = face->mIndices[i]; 
+			for (int i = 0; i < face->mNumIndices; i++)
+			{
+				int vertexIndex = face->mIndices[i];
 
-				if(mesh->HasVertexColors(0))
-					glColor4fv((GLfloat*)&mesh->mColors[0][vertexIndex]);
-
+				if (mesh->HasVertexColors(0))
+					glColor4fv((GLfloat *)&mesh->mColors[0][vertexIndex]);
 
 				if (mesh->HasNormals())
 					glNormal3fv(&mesh->mNormals[vertexIndex].x);
@@ -168,7 +172,6 @@ void render (const aiScene* sc, const aiNode* nd)
 
 			glEnd();
 		}
-
 	}
 
 	// Draw all children
@@ -181,33 +184,107 @@ void render (const aiScene* sc, const aiNode* nd)
 //--------------------OpenGL initialization------------------------
 void initialise()
 {
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_NORMALIZE);
 	glLightModeli(GL_LIGHT_MODEL_TWO_SIDE, GL_TRUE);
 	glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
-	loadModel("truck.3ds");			//<<<-------------Specify input file name heres
+	loadModel("Model1_BVH/Boxing.bvh"); //<<<-------------Specify input file name heres
 	//loadGLTextures(scene);        //<<<-------------Uncomment when implementing texturing
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45, 1, 1.0, 1000.0);
 }
 
-//----Timer callback for continuous rotation of the model about y-axis----
-void update(int value)
+// Find the keyframes that the animation is currently between, and produce an interpolated rotation.
+aiMatrix4x4 get_interpolated_rotation(double tick, aiNodeAnim *node)
 {
-	angle++;
-	if(angle > 360) angle = 0;
+	aiQuatKey prevKey;
+	aiQuatKey nextKey;
+	for (int frame = 0; frame < node->mNumRotationKeys; frame++)
+	{
+		if (tick > node->mRotationKeys[frame].mTime)
+		{
+			prevKey = node->mRotationKeys[frame];
+			continue;
+		}
+
+		nextKey = node->mRotationKeys[frame];
+
+		double timeDifference = nextKey.mTime - prevKey.mTime;
+		double currentTime = tick - prevKey.mTime;
+		double timeLerp = currentTime / timeDifference;
+
+		aiQuaternion outRotation;
+		aiQuaternion::Interpolate(outRotation, prevKey.mValue, nextKey.mValue, timeLerp);
+		return aiMatrix4x4(outRotation.GetMatrix());
+	}
+	// if there is only 1 keyframe, return its value
+	return aiMatrix4x4(prevKey.mValue.GetMatrix());
+}
+
+// Find the keyframes that the animation is currently between, and produce an interpolated rotation.
+aiMatrix4x4 get_interpolated_position(double tick, aiNodeAnim *node)
+{
+	aiVectorKey prevKey;
+	aiVectorKey nextKey;
+	for (int frame = 0; frame < node->mNumPositionKeys; frame++)
+	{
+		if (tick > node->mPositionKeys[frame].mTime)
+		{
+			prevKey = node->mPositionKeys[frame];
+			continue;
+		}
+
+		nextKey = node->mPositionKeys[frame];
+
+		double timeDifference = nextKey.mTime - prevKey.mTime;
+		double currentTime = tick - prevKey.mTime;
+		double timeLerp = currentTime / timeDifference;
+
+		aiVector3D outPosition;
+		outPosition = prevKey.mValue + (nextKey.mValue - prevKey.mValue) * float(timeLerp);
+
+		aiMatrix4x4 positionMatrix;
+		aiMatrix4x4::Translation(outPosition, positionMatrix);
+		return positionMatrix;
+	}
+	// if there is only 1 keyframe, return its value
+	aiMatrix4x4 positionMatrix;
+	aiMatrix4x4::Translation(prevKey.mValue, positionMatrix);
+	return positionMatrix;
+}
+
+//----Timer callback for continuous rotation of the model about y-axis----
+void update(int millisSinceStart)
+{
+	aiAnimation *anim = scene->mAnimations[0];
+
+	double tick = fmod((millisSinceStart * anim->mTicksPerSecond) / 1000.0, anim->mDuration);
+
+	for (int i = 0; i < anim->mNumChannels; i++)
+	{
+		aiNodeAnim *node = anim->mChannels[i];
+
+		aiMatrix4x4 rotationMatrix = get_interpolated_rotation(tick, node);
+		aiMatrix4x4 postitionMatrix = get_interpolated_position(tick, node);
+
+		aiNode *skeletonNode = scene->mRootNode->FindNode(node->mNodeName);
+
+		skeletonNode->mTransformation = postitionMatrix * rotationMatrix;
+	}
+
 	glutPostRedisplay();
-	glutTimerFunc(50, update, 0);
+	glutTimerFunc(10, update, millisSinceStart + 10);
 }
 
 //----Keyboard callback to toggle initial model orientation---
 void keyboard(unsigned char key, int x, int y)
 {
-	if(key == '1') modelRotn = !modelRotn;   //Enable/disable initial model rotation
+	if (key == '1')
+		modelRotn = !modelRotn; //Enable/disable initial model rotation
 	glutPostRedisplay();
 }
 
@@ -224,57 +301,32 @@ void display()
 	gluLookAt(0, 0, 3, 0, 0, -5, 0, 1, 0);
 	glLightfv(GL_LIGHT0, GL_POSITION, pos);
 
-	glRotatef(angle, 0.f, 1.f ,0.f);  //Continuous rotation about the y-axis
-	if(modelRotn) glRotatef(-90, 1, 0, 0);		  //First, rotate the model about x-axis if needed.
-
-	// scale the whole asset to fit into our view frustum 
+	// scale the whole asset to fit into our view frustum
 	float tmp = scene_max.x - scene_min.x;
-	tmp = aisgl_max(scene_max.y - scene_min.y,tmp);
-	tmp = aisgl_max(scene_max.z - scene_min.z,tmp);
+	tmp = aisgl_max(scene_max.y - scene_min.y, tmp);
+	tmp = aisgl_max(scene_max.z - scene_min.z, tmp);
 	tmp = 1.f / tmp;
 	glScalef(tmp, tmp, tmp);
 
-	float xc = (scene_min.x + scene_max.x)*0.5;
-	float yc = scene_min.y;
-	float zc = (scene_min.z + scene_max.z)*0.5;
-	// center the model
-	glTranslatef(-xc, -yc, -zc);
-
-        // if the display list has not been made yet, create a new one and
-        // fill it with scene contents
-	if(scene_list == 0)
-	{
-	    scene_list = glGenLists(1);
-	    glNewList(scene_list, GL_COMPILE);
-            // now begin at the root node of the imported data and traverse
-            // the scenegraph by multiplying subsequent local transforms
-            // together on GL's matrix stack.
-	       render(scene, scene->mRootNode);
-	    glEndList();
-	}
-
-	glCallList(scene_list);
+	render(scene, scene->mRootNode);
 
 	glutSwapBuffers();
 }
 
-
-
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(600, 600);
 	glutCreateWindow("Model Loader");
-	glutInitContextVersion (4, 2);
-	glutInitContextProfile ( GLUT_CORE_PROFILE );
+	glutInitContextVersion(4, 2);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
 
 	initialise();
 	glutDisplayFunc(display);
-	glutTimerFunc(50, update, 0);
+	glutTimerFunc(50, update, 50);
 	glutKeyboardFunc(keyboard);
 	glutMainLoop();
 
 	aiReleaseImport(scene);
 }
-
